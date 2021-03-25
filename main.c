4324c715a23831ac0a6c78eaada3375e9ea8e93d
OEM Configuration (temporary user) <a95354@alunos.uminho.pt>
2021-03-25 16:38:34 +0000

    Implementada função main

---
+++ main.c
@@ -0,0 +1,38 @@
+/**
+ *
+ * @file Contém a definição da função main
+ *
+ *
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include "parser.h"
+
+/**
+ *
+ * O ponto de entrada da aplicação.
+ * 
+ */
+int main() {
+    char *line = NULL;
+	size_t size = 0;
+	
+    //Usa-se getline porque o scanf para de ler ao encontrar um
+    //espaço.
+    size_t read = getline(&line, &size,stdin);
+
+    //getline lê o caracter '\n', logo coloca-se u, '\0' no seu lugar
+    //para não interferir no processamento
+    line[read - 1] = '\0';
+
+    struct stack st = empty();
+
+    processInput(line, &st);
+    printStackLine(&st);
+    
+    return 0;
+}

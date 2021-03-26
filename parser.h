945759287066643b6eaffceb65c0170229604666
Rui Oliveira <rpoliveira02@gmail.com>
2021-03-26 10:48:54 +0000

    Mudada documentação para o ficheiro .c

--- parser.h
+++ parser.h
@@ -1,7 +1,3 @@
-#ifndef PARSER_H
-/*! Include guard */
-#define PARSER_H
-
 /**
  * 
  * @file contém as definições das funções usadas para processar o input e 
@@ -9,83 +5,27 @@
  * 
  */
 
+#ifndef PARSER_H
+/*! Include guard */
+#define PARSER_H
+
+
 #include "stack.h"
 
-/**
- * \brief Verifica se o caracter fornecido é um algarismo.
- * 
- * @param ch O caracter fornecido
- * @return   Se for um algarismo, 1, senão, 0
- */
 bool isNumeric(char ch);
 
-/**
- * \brief Calcula o número de operandos exigidos pela operação definida pelo
- * caracter dado.
- * 
- * @param ch O caracter fornecido
- * @return   O número de operandos exigidos pela respetiva operação (-1 caso
- *           o caracter não codifique nenhuma operação)
- */
 int numberOperands (char ch);
 
-/**
- * \brief Executa a operação definida pelo caracter dado, a qual exige dois operandos,
- * x e y, na stack fornecida.
- * 
- * @param st A stack onde executar a operação
- * @param x  O primeiro operando
- * @param y  O segundo operando
- * @param ch O caracter que define a operação a executar
- */
 void twoOperand(struct stack* st, int x, int y, char ch);
 
-/**
- * \brief Executa a operação definida pelo caracter dado, a qual exige um operando x,
- * na stack fornecida.
- * 
- * @param st A stack onde executar a operação
- * @param x  O operando
- * @param ch O caracter que define a operação a executar
- */
 void oneOperand(struct stack* st, int x, char ch);
 
-/**
- * \brief Executa a operação definida pelo caracter dado na stack fornecida.
- * 
- * @param st A stack onde executar a operação
- * @param ch O caracter que define a operação a executar
- */
 void operation (struct stack* st, char ch);
 
-/**
- * \brief Processa a string fornecida, e preenche a stack dada, efetuando todas as operações descritas na string.
- * 
- * @param str   A string correspondente ao input
- * @param st    A stack a preencher
- */
 void processInput(char* str, struct stack* st);
 
-
-/**
- * \brief Imprime a stack fornecida para o ecrã. 
- * 
- * De notar que esta função retira todos os elementos da mesma, resultando
- * uma stack vazia.
- * 
- * @param st   A stack a imprimir
- */
 void printStack(struct stack* st);
 
-
-/**
- * \brief Imprime a stack fornecida para o ecrã e muda de linha. 
- * 
- * De notar que esta função retira todos os elementos da mesma, resultando
- * uma stack vazia.
- * 
- * @param st   A stack a imprimir
- */
 void printStackLine(struct stack* st);
 
 #endif

aa2c2e2ffa7427161d07b9f33f496ff4d75d338d
Renato Martins <73346623+Renatotmmartins@users.noreply.github.com>
2021-05-04 21:52:26 +0100

    Adicionadas funções relacionadas com os blocos e respetiva declaração e documentação.

--- blockOperations.c
+++ blockOperations.c
@@ -1,1 +1,32 @@
+/**
+* @file contém a implementação das funções correspondentes
+        * às operações relacionadas com blocos
+*/
 
+#include "stack.h"
+#include "logicOperations.h"
+
+/**
+ * \brief Executa um bloco dentro de uma stack
+ * @param st a stack fornecida
+ * @param block bloco fornecido
+ * @return Value que é resultado da operação do bloco dentro da stack
+ */
+
+Value execute (Stack st, Value block) {
+    State s;
+    s -> Stack = st;
+    processInput (&block.block, &s);
+    return st -> value;
+}
+
+/**
+ * \brief Executa um bloco dentro de uma stack enquanto houver um valor verdadeiro no topo da stack
+ * @param st a stack fornecida
+ * @param block bloco fornecido
+ */
+
+Void execute_while_true (Stack st, Value block) {
+    while (!isEmpty(st) && isTrue(st -> value))
+        execute (st, block);
+}
--- blockOperations.h
+++ blockOperations.h
@@ -1,1 +1,19 @@
+/**
+ * @file contém a declaração das funções correspondentes
+ * às operações relacionadas com os blocos
+ */
 
+//! Include guard
+#ifndef BLOCK_OPERATIONS_H
+//! Include guard
+#define BLOCK_OPERATIONS_H
+
+#include "stack.h"
+#include "logicOperations.h"
+
+Value execute (Stack st, Value block);
+
+Void execute_while_true (Stack st, Value block);
+
+
+#endif
